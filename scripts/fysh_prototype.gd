7e6b1c636bdc2180f0abec047af29fd26b0e88fe
ZiyadAziz <ziyad.abdelaziz03@gmail.com>
2026-07-09 15:13:56 -0400

    future planning

--- scripts/fysh_prototype.gd
+++ scripts/fysh_prototype.gd
@@ -3,6 +3,12 @@ extends Area2D
 #@onready var level_manager: Node = %LevelManager
 @onready var level_manager = get_node("/root/Game/LevelManager")
 
+#The vision cone for the fysh might either be a raycast or area2d 
+# or a secret third thing that a youtube video tells me
+
+var SPEED := 50.0
+var direction = 1 #We want to change the direction when the fysh collides with the wall or when it sees a bobber
+
 #Not sure if this is the best method of "catching" a fysh since the cursor is going to be the tool too
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
@@ -11,3 +17,10 @@ func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 		#but since theres gonna be multiple tools idk if the script should be on the fysh itself or its own seperate thing
 		level_manager.add_point()
 		queue_free()
+
+func _process(delta: float) -> void:
+	#have it roam around; see if theres a common roaming around function
+	# also this will be an fsm where it will roam, but when it sees a bobber
+	# it might get interested and start moving towards it instead
+	#position.x += SPEED * delta * direction
+	pass
