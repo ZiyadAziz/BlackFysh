7e1dec7bd42186f1aa8850848bee512faf2418c7
ZiyadAziz <ziyad.abdelaziz03@gmail.com>
2026-07-12 13:33:27 -0400

    obber

---
+++ scripts/bobber_prototype.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+#Not sure what needs to be here...
+#maybe just a _process func that just looks for a click
+# and if the player clicks, it takes away the bobber
+# if a fish is hooked, then it initiates the minigame
+# otherwise it just goes away
